e93bc5fc7bb961e41f3f40c544db8817114d7a70
Victor-br-santo <victorbrsanto@Gmail.com>
2025-08-03 18:09:50 -0300

    modified:   server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
-const inscricaoRoutes = require('.routes/inscricao');
+const inscricaoRoutes = require('./routes/inscricao');
 
 const app = express();
 const router = express.Router();
