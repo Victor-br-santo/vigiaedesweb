d333abc0b14df6a6d534b835aa7b37c064d7f4d6
Victor-br-santo <victorbrsanto@Gmail.com>
2025-08-04 15:12:24 -0300

    modified:   server.js

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Middleware JWT para verificar token
 function verificarToken(req, res, next) {
+  console.log("Authorization header:", req.headers.authorization);
   const authHeader = req.headers["authorization"];
   console.log("Authorization header:", authHeader);  // <<< log para debug
   const token = authHeader && authHeader.split(" ")[1]; // Bearer <token>
